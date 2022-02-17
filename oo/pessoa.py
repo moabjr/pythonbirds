e2d6deebc5b24d493577614f3e3424a7b5ecbe89
moabjr <moabjr2@gmail.com>
2022-02-17 16:53:30 -0300

    Criados atributo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=41):
+    def __init__(self, *filhos, nome=None, idade=41):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Moab'
-    print(p.nome, p.idade)
+    renzo = Pessoa(nome='Renzo')
+    moab = Pessoa(renzo,nome='Moab')
+    print(Pessoa.cumprimentar(moab))
+    print(id(moab))
+    print(moab.cumprimentar())
+    print(moab.nome, moab.idade)
+    for filho in moab.filhos:
+        print('O nome do filho do ' + moab.nome +' é ' + filho.nome)
